329996b376b88666f99d7a1be0f3badca91d73dc
Vicky Gupta <vckgupta008@gmail.com>
2016-08-08 14:11:00 +0530

    replaced

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,133 +1,210 @@
-$('document').ready(function() {
-    $("#add").hide();
-    var temp = {};
-    $("#read").on('click', function() {
-        $("#add").hide();
-        $("#tcontent").empty();
-        $("#tcontent").show();
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:8080/player",
+var myApp = angular.module('myModule', []);
 
-            success: function(data) {
-                if (data[0]) {
-                    $("<thead></thead>", {
-                        class: 'thead-inverse'
-                    }).appendTo("#tcontent");
+myApp.controller("myController", function($scope, $http) {
+    $scope.start = 0;
+    $scope.start2 = 0;
+    $scope.IsVisible = false;
+     // $scope.IsVisible2 = false;
+    $scope.showhide = function(val) {
+        
+         // $scope.IsVisible2 = false;
+         $scope.Name="";
+         $scope.Company="";
+          $scope.Email="";
+          $scope.IsVisible = $scope.IsVisible ? false : true; 
+         if(val==1)
+         {
+         $scope.showButton=true;
+     }
 
-                    $("<tr></tr>").appendTo("#tcontent>thead");
+    }
 
-                    var key = Object.keys(data[0])
-                        //console.log(key);
-                    for (var i = 0; i < key.length; i++) {
-                        $("<th>" + key[i] + "</th>").appendTo("#tcontent>thead>tr");
-                    }
-                    $("<th colspan=" + "'2'" + ">Operations</th>").appendTo("#tcontent>thead>tr");
-                    console.log(data);
-                }
-                $("<tbody></tbody>").appendTo("#tcontent");
-                for (var i = 0; i < data.length; i++) {
-                    //console.log(key.id);
-                    var htm = "<tr id=" + data[i].id + "><td>" + data[i].id + "</td><td>" + data[i].name + "</td><td>" + data[i].email + "</td><td>" + data[i].phone + "</td> <td><a href=" + "'#myModal'" + "role=" + "'button'" + "class=" + "'btn btn-warning'" + "data-toggle=" + "'modal'" + ">Update</a></td><td><button id=" + "'delete'" + " class=" + "'btn btn-danger'" + ">Delete</button></td></tr>";
-                    $("#tcontent>tbody").append(htm);
-
-                    // $("<tr><td>"+data.id+"</tr></td>").appendTo("#tcontent>tbody");
-                    // $("<td>"+data.name+"</td>"+"<td>"+data.email+"</td>"+"<td>"+data.phone+"</td>").appendTo("#tcontent>tbody>tr");          
-                }
-            }, //end success function   
-            error: function(e) {
-                alert("fail to load");
+    
+    $scope.StartI=function(start1){
+    	$scope.start += start1;
+            if ($scope.start >= 0) 
+            {
+            	if($scope.type=="read")
+            	{
+            		$scope.read($scope.start);
+            	}
+            	else
+            	{
+            		$scope.sort($scope.start);
+            	}
             }
-        }); //end ajax
-    }); //end read
-    $('#tcontent')
-        .delegate('#delete', 'click', function() {
-            var $this = $(this).parent().parent();
-            var id = $this.children("td:first").text();
-            console.log(id);
-            $.ajax({
+            else
+            {
+            	$scope.start += 50;
+            	alert("Already at the beginning");
+            }
+    }
+    $scope.read = function(start1) 
+    {	$scope.type="read";
+            $scope.IsVisible = false;
+            $scope.IsVisible2 = false;
+            
+                $http({
 
-                url: "http://localhost:8080/player/" + id,
-                method: "DELETE",
-                success: function(data) {
-                    alert("successfully deleted");
-                    $this.remove();
-                }
-            }); //end ajax
-        }); //end delegate delete
+                        method: "GET",
+                        url: "http://localhost:8081/player?_start=" + start1 + "&_limit=50",
+                    })
+                    .success(function(response) {
+                        console.log(response);
+                        $scope.players = response;
+                    })
+                    .error(function(response) {
+                        alert("Failed to load");
 
-    $('#tcontent').delegate('a', 'click', function() {
-        var $this = $(this).parent().parent();
-        var id = $this.children("td:first").text();
-        var name = $this.children("td").eq(1).text();
-        var email = $this.children("td").eq(2).text();
-        var phone = $this.children("td").eq(3).text();
-        //alert(name);  
-        $('div#myModal div.modal-body input#id').val(id);
-        $('div#myModal div.modal-body input#inputName').val(name);
-        $('div#myModal div.modal-body input#email').val(email);
-        $('div#myModal div.modal-body input#phone').val(phone);
-
-        $('#mbody').submit(function(e) {
-            //alert("after submit");
-            temp = {};
-            id = $('div#myModal div.modal-body input#id').val()
-            name = $('div#myModal div.modal-body input#inputName').val();
-            email = $('div#myModal div.modal-body input#email').val();
-            phone = $('div#myModal div.modal-body input#phone').val();
-            temp["id"] = id;
-            temp["name"] = name;
-            temp["email"] = email;
-            temp["phone"] = phone;
-            $.ajax({
-                type: "PUT",
-                //dataType: 'json', 
-                url: "http://localhost:8080/player/" + id,
-                data: JSON.stringify(temp),
-                contentType: "application/json"
-            }); //end ajax
-            $('#myModal').modal('hide');
-            var row = $("#tcontent tbody tr#" + id);
-            row.children("td").eq(1).text(name);
-            row.children("td").eq(2).text(email);
-            row.children("td").eq(3).text(phone);
-            // $('#tcontent tr#')
-            //$("#tcontent").empty();   
-            // $("#tcontent").hide();   
-            alert("Successfully updated");
-            e.preventDefault();
-        }); //end update
+                    });
+             
+        } //end read
 
 
 
-    }); //end #tcontent
-    $("#b_add").click(function() {
-        $("#add").show();
-        $("#tcontent").hide();
-    });
-    $('#add').submit(function(e) {
-        temp = {};
-        alert("inside submit");
-        //var id = $('#add').find('input[name="id"]').val();
-        var name = $('form#add input#inputName').val();
-        var email = $('form#add input#email').val();
-        var phone = $('form#add input#phone').val();
-        temp["name"] = name;
-        temp["email"] = email;
-        temp["phone"] = phone;
-        // json.push(temp);
-        // alert(json);
+    $scope.delete = function(id, index) {
+            $http({
 
-        $.ajax({
-            type: "POST",
-            // dataType: 'json', 
-            url: "http://localhost:8080/player",
-            data: JSON.stringify(temp),
-            contentType: "application/json"
-        });
-        alert("successfully added");
-        $('#add').hide();
-        e.preventDefault();
-    });
+                    method: "DELETE",
+                    url: "http://localhost:8081/player/" + id
+                })
+                .success(function(response) {
+                    $scope.players.splice(index, 1);
+                    alert("Successfully deleted");
+                })
+                .error(function(response) {
+                    alert("Failed to delete");
+                    $scope.error = response.statusText;
+                });
+        } //end delete
+    $scope.add = function() 
+    {	
+    		$scope.start = 0;
+            $scope.IsVisible = false;
+            
+            var player = { name: $scope.Name, company: $scope.Company, email: $scope.Email };
+            console.log(player);
+            $http({
+                    method: "POST",
+                    url: "http://localhost:8081/player",
+                    data: player
+                })
+                .success(function(response) {
+                    //$scope.players.push(response.data);
+                    console.log(response);
+                    // $scope.players=$scope.players.push(response);
+                    alert("Successfully");
+                })
+                .error(function(response) {
+                    alert("Failed to add");
+                    $scope.error = response.statusText;
+                });
 
-}); //end document
+        } //end add
+    $scope.search = function(value) 
+    {		
+    	$scope.start = 0;			
+            $scope.IsVisible = false;
+          
+            alert(value);
+            $http({
+                    method: "GET",
+                    url: "http://localhost:8081/player?q=" + value,
+
+                })
+                .success(function(response) {
+                    if (response == "") {
+
+                        alert("No records found");
+                    } else {
+                        alert(response.length + " No of records are found");
+                        $scope.players = response;
+                    }
+                })
+                .error(function(response) {
+                    alert("Failed to Search");
+
+                });
+        } //end search
+
+$scope.sort = function(start1) 
+{
+$scope.type="sort";		
+
+            $scope.IsVisible = false;
+            
+           
+                $http({
+
+                        method: "GET",
+                        url: "http://localhost:8081/player?_sort=" + $scope.sortVal + "&_order=" + $scope.orderVal + "&_start=" + start1 + "&_limit=50",
+                        //url: "http://localhost:8081/player?_start=" + $scope.start + "&_limit=50",
+                    })
+                    .success(function(response) {
+                        console.log(response);
+                        $scope.players = response;
+                    })
+                    .error(function(response) {
+                        alert("Failed to load");
+
+                    });
+            
+        } //end sort
+     $scope.update = function(player,index) 
+     {
+        $scope.IsVisible = $scope.IsVisible ? false : true;
+        $scope.showButton=false;
+        //$scope.player=player;
+        $scope.Id=player.id;
+        $scope.Name=player.name;
+        $scope.Company=player.company;
+        $scope.Email=player.email;
+        $scope.index=index;
+    }
+    $scope.save=function(){
+    	$scope.IsVisible=false;
+    	var player = { name: $scope.Name, company: $scope.Company, email: $scope.Email };
+    	 $http({
+                method: "PUT",
+                url: "http://localhost:8081/player/" + $scope.Id,
+                headers: { 'Content-Type': 'application/json' },
+                data: player
+            }).then(function mySucces(response) {
+                
+                $scope.IsVisible2 = false;
+
+                $scope.players[$scope.index].id=$scope.Id;
+                $scope.players[$scope.index].name=$scope.Name;
+                $scope.players[$scope.index].company=$scope.Company;
+                $scope.players[$scope.index].email=$scope.Email;
+                alert('successfully updated');
+            }, function myError(response) {
+                alert('Failed to update');
+            });
+    }//end save
+
+    // $scope.open = function(size, selectedPlayer) {
+
+    //     var modalInstance = $modal.open({
+    //         animation: $scope.animationsEnabled,
+    //         templateUrl: 'edit.html',
+    //         controller: function($scope, $modalInstance, player) {
+    //             $scope.player = player;
+    //         },
+    //         size: size,
+    //         resolve: {
+    //             player: function() {
+    //                 return $scope.selectedPlayer;
+    //             }
+    //         }
+    //     });
+
+    //     modalInstance.result.then(function(selectedItem) {
+    //         $scope.selected = selectedItem;
+    //     }, function() {
+    //         $log.info('Modal dismissed at: ' + new Date());
+    //     });
+    // };//end open
+
+
+}); //end controller
